1f527198f4854a7f667c2a7d16ba0cff0ebf52c0
Reinaldo <reiguitarra@gmail.com>
2020-01-17 00:04:59 -0300

    Criado classe tabuleiro

--- JogoXadrez/Program.cs
+++ JogoXadrez/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using tabuleiro;
 
 namespace JogoXadrez
 {
@@ -6,7 +7,10 @@ namespace JogoXadrez
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Posicao p = new Posicao(3, 4);
+
+
+            Console.WriteLine(p);
         }
     }
 }
---
+++ JogoXadrez/tabuleiro/Posicao.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace tabuleiro
+{
+    class Posicao
+    {
+        public int Linha { get; set; }
+        public int Coluna { get; set; }
+
+        public Posicao()
+        {
+
+        }
+
+        public Posicao(int linha, int coluna)
+        {
+            Linha = linha;
+            Coluna = coluna;
+        }
+
+        public override string ToString()
+        {
+            return "Posição  Linha : " + Linha + " Coluna : " + Coluna;
+        }
+    }
+}
